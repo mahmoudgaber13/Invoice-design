3ddb31b9819d0c136e01ee429b325440f4302fcc
mahmoudgaber13 <mahmoud.gaber.1393@gmail.com>
2020-07-11 12:40:54 +0200

    first version

---
+++ script.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+	var Items = ' <select name="Item"   class="form-control"><option value="Item1">Item1</option><option value="Item2">Item2</option><option value="Item3">Item3</option></select>'
+    var i=1;
+    $("#add_row").click(function(){b=i-1;
+      	$('#addr'+i).html($('#addr'+b).html()).find('td:first-child').html(Items);
+      	$('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
+      	i++; 
+  	});
+    $("#delete_row").click(function(){
+    	if(i>1){
+		$("#addr"+(i-1)).html('');
+		i--;
+		}
+		calc();
+	});
+	
+	$('#tab_logic tbody').on('keyup change',function(){
+		calc();
+	});
+	$('#tax').on('keyup change',function(){
+		calc_total();
+	});
+	
+
+});
+
+function calc()
+{
+	$('#tab_logic tbody tr').each(function(i, element) {
+		var html = $(this).html();
+		if(html!='')
+		{
+			var qty = $(this).find('.qty').val();
+			var price = $(this).find('.price').val();
+			var discount = $(this).find('.discount').val();
+			var total = qty*price;
+			$(this).find('.total').val(qty*price);
+			$(this).find('.net').val(total-discount);
+			calc_total();
+		}
+    });
+}
+
+function calc_total()
+{
+	total=0;
+	$('.net').each(function() {
+        total += parseInt($(this).val());
+    });
+	$('#sub_total').val(total.toFixed(2));
+	tax_sum=total/100*$('#tax').val();
+	$('#tax_amount').val(tax_sum.toFixed(2));
+	$('#total_amount').val((tax_sum+total).toFixed(2));
+}
